3c4a6da9ef2c0eb155681a45466921f78a0d8527
AlexxMotta <alexmota@alu.ufc.br>
2023-03-14 18:35:54 -0300

    add rota show

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ use App\http\Controllers\EventController;
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
 Route::post('/events', [EventController::class, 'store']);
+Route::get('/events/{id}', [EventController::class, 'show']);
 
